c9256d8486ab2af5672671b8a78428f1642361fe
Wlubster <62694383+Wlubster@users.noreply.github.com>
2020-04-29 14:36:15 +0200

    Create Zad5Lab5.py

---
+++ Zad5Lab5.py
@@ -0,0 +1,9 @@
+a = int (input("Podaj liczbe a: "))
+b = int (input("Podaj liczbe b: "))
+c = int (input("Podaj liczbe c: "))
+
+
+if 10>a>0 and a>b or b>c and 10>a>0:
+    print ("warunki są spełnione")
+else:
+    print("Warunki nie są spełnione!")
