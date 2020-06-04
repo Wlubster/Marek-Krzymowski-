f7827a7c7a07c815ebf7b308e1213497f8a6a42b
Wlubster <62694383+Wlubster@users.noreply.github.com>
2020-06-04 16:52:36 +0200

    Create Zad2Lab5.py

---
+++ Zad2Lab5.py
@@ -0,0 +1,7 @@
+import sys
+print("Podaj pierwszą liczbę: ")
+x = int(sys.stdin.readline())
+print("Podaj drugą liczbę: ")
+y = int(sys.stdin.readline())
+
+sys.stdout.write("Iloczyn wynosi: " + str(x*y))
