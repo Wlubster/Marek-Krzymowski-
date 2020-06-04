9c5b712673edba04f8f9a0e496acd863a993ec45
Wlubster <62694383+Wlubster@users.noreply.github.com>
2020-06-04 16:53:11 +0200

    Create Zad3Lab5.py

---
+++ Zad3Lab5.py
@@ -0,0 +1,8 @@
+a, b = 3, 5
+
+print("a == b " + str(bool(a == b)))
+print("a != b " + str(bool(a != b)))
+print("a > b " + str(bool(a > b)))
+print("a < b " + str(bool(a < b)))
+print("a >= b " + str(bool(a >= b)))
+print("a <= b " + str(bool(a <= b)))
